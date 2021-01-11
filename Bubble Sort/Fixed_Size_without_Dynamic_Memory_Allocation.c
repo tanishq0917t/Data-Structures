c0f20ddd878deb713b07938198ad56fb19841760
Tanishq Rawat <67191912+tanishq-17102001@users.noreply.github.com>
2021-01-11 20:46:17 +0530

    Update Fixed_Size_without_Dynamic_Memory_Allocation.c

--- Bubble Sort/Fixed_Size_without_Dynamic_Memory_Allocation.c
+++ Bubble Sort/Fixed_Size_without_Dynamic_Memory_Allocation.c
@@ -1,1 +1,33 @@
-
+#include<stdio.h>
+int main()
+{
+int x[5],e,f,g,m=3;
+for(int e=0;e<5;e++)
+{
+printf("Enter the number: ");
+scanf("%d",&x[e]);
+}
+while(m>=0)
+{
+e=0;
+f=1;
+while(e<=m)
+{
+if(x[f]<x[e])
+{
+g=x[e];
+x[e]=x[f];
+x[f]=g;
+}
+e++;
+f++;
+}
+m--;
+}
+printf("After Sorting\n");
+for(int e=0;e<5;e++)
+{
+printf("%d\n",x[e]);
+}
+return 0;
+}
