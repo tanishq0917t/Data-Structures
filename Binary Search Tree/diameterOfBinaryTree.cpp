b822780702fb6e56f8602b3b75d7dbb09a39a5d6
Tanishq Rawat <67191912+tanishq-17102001@users.noreply.github.com>
2022-02-09 21:02:10 +0530

    Update diameterOfBinaryTree.cpp

--- Binary Search Tree/diameterOfBinaryTree.cpp
+++ Binary Search Tree/diameterOfBinaryTree.cpp
@@ -1,94 +1,85 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 typedef struct _bstNode
 {
-int data;
-struct _bstNode *left;
-struct _bstNode *right;
-}BSTNode;
-BSTNode *root=NULL;
-BSTNode * createNode()
+    int data;
+    struct _bstNode *left;
+    struct _bstNode *right;
+} BSTNode;
+BSTNode *root = NULL;
+BSTNode *createNode()
 {
-BSTNode *t;
-t=(BSTNode *)malloc(sizeof(BSTNode));
-t->left=NULL;
-t->right=NULL;
-return t;
-}
-int dia=INT_MIN;
-void insertNode(int data)
-{
-BSTNode *t,*j;
-t=createNode();
-t->data=data;
-if(!root){root=t;return;}
-j=root;
-while(1)
-{
-if(t->data>j->data) //right case
-{
-if(j->right==NULL) //adding node to end
-{
-j->right=t;
-break;
-}
-else
-{
-j=j->right;  //iterating over right node
-}
-}
-else  //left case
-{
-if(j->left==NULL) //adding node to end
-{
-j->left=t;
-break;
-}
-else  //iterating over left node
-{
-j=j->left;
-}
-}
-}
+    BSTNode *t;
+    t = (BSTNode *)malloc(sizeof(BSTNode));
+    t->left = NULL;
+    t->right = NULL;
+    return t;
 }
 
-void inorder(BSTNode *root)
+void insertNode(int data)
 {
-if(root==NULL) return;
-inorder(root->left);
-cout<<root->data<<endl;
-inorder(root->right);
+    BSTNode *t, *j;
+    t = createNode();
+    t->data = data;
+    if (!root)
+    {
+        root = t;
+        return;
+    }
+    j = root;
+    while (1)
+    {
+        if (t->data > j->data) // right case
+        {
+            if (j->right == NULL) // adding node to end
+            {
+                j->right = t;
+                break;
+            }
+            else
+            {
+                j = j->right; // iterating over right node
+            }
+        }
+        else // left case
+        {
+            if (j->left == NULL) // adding node to end
+            {
+                j->left = t;
+                break;
+            }
+            else // iterating over left node
+            {
+                j = j->left;
+            }
+        }
+    }
 }
-int max_height = INT_MIN;
-    int height(BSTNode* root){
-        if(root==NULL){
-            return 0;
-        }
-        
-        int heightLeft=0, heightRight=0;
-        if(root->left!=NULL){
-            heightLeft = height(root->left);
-        }
-        if(root->right!=NULL){
-            heightRight = height(root->right);
-        }
-        
-        int curMax = max({heightLeft, heightRight, heightLeft+heightRight });
-        max_height= max(curMax, max_height);
-        return max(heightLeft, heightRight)+1;
-    }
-    int diameterOfBinaryTree(BSTNode* root) {
-        height(root);
-        return max_height;
-    }
+int height(BSTNode *root,int &d)
+{
+    if(!root) return 0;
+    int lh=height(root->left,d);
+    int rh=height(root->right,d);
+    d=max(d,lh+rh);
+    return 1+max(lh,rh);
+}
+int diameter(BSTNode *root)
+{
+    int d=0;
+    height(root,d);
+    return d;
+}
 int main()
 {
-insertNode(100);
-insertNode(50);
-insertNode(25);
-insertNode(90);
-insertNode(75);
-insertNode(200);
-cout<<"Diameter of tree is: "<<diameterOfBinaryTree(root)+1<<endl;
-return 0;
+    insertNode(5);
+    insertNode(3);
+    insertNode(4);
+    insertNode(6);
+    insertNode(2);
+    insertNode(1);
+    insertNode(7);
+    insertNode(9);
+    insertNode(8);
+    cout<<"Diameter of Tree is: "<<diameter(root)<<endl;
+    return 0;
 }
